3055768c2a4c14e8862eee357ac3423ce147312c
Calise425 <caitlin.norris1998@gmail.com>
2024-02-26 18:47:26 -0700

    snake functionality finished. could polish: frame rate, interaction with quickly changing directions

--- main.py
+++ main.py
@@ -1,20 +1,52 @@
-from turtle import Turtle, Screen
-import time
+from turtle import Screen
 from snake import Snake
+from food import Food
+from scoreboard import ScoreBoard
+import time
 
 screen = Screen()
-screen.setup(width=600, height=600)
+screen.setup(width=620, height=620)
 screen.bgcolor("black")
 screen.title("Snake")
 screen.tracer(0)
 
+food = Food()
 snake = Snake()
+scoreboard = ScoreBoard()
 
+screen.listen()
+screen.onkey(snake.up, key="Up")
+screen.onkey(snake.down, key="Down")
+screen.onkey(snake.left, key="Left")
+screen.onkey(snake.right, key="Right")
+
+# Gameplay loop that moves the snake and food, and updates score
 game_on = True
 while game_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+
+    # Detect collision with food
+    if snake.head.distance(food) < 5:
+        food.move()
+        snake.extend()
+        scoreboard.increase_score()
+
+    # Detect collision with wall
+    if (snake.head.xcor() > 300
+            or snake.head.xcor() < -300
+            or snake.head.ycor() > 300
+            or snake.head.ycor() < -300):
+        game_on = False
+        scoreboard.game_over()
+        break
+
+    # Detect collision with tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_on = False
+            scoreboard.game_over()
 
 
 screen.exitonclick()
